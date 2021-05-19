ae76ca97d7b2fd4205666ac2c14a32d03479f66b
Bogdan <bogdan@mail.ru>
2021-05-19 19:48:17 +0300

    create first template

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def main_page(request):
-    return HttpResponse("hello, this is main page")
-# Create your views here.
+    return render(request, "sales_manager/index.html")
+
